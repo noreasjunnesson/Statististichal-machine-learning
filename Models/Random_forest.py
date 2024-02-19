ea275f589f69814ee1ec40ccf6aad857aa0363c4
noreasjunnesson <160476163+noreasjunnesson@users.noreply.github.com>
2024-02-19 13:21:32 +0100

    Update Random_forest.py

--- Models/Random_forest.py
+++ Models/Random_forest.py
@@ -1,4 +1,14 @@
-
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sklearn.linear_model as skl_lm
+from sklearn.metrics import confusion_matrix, roc_curve, auc, f1_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold, GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint
 class random_forest:
     def train(self, X_train,Y_train):
 
@@ -21,7 +31,7 @@ class random_forest:
 
         
 
-        def grid_search(X_train,Y_train):
+        def gridsearch(X_train,Y_train):
             param_grid = {'n_estimators': [100],
                         'max_depth': [ 5, 10,20],
                         'criterion': ['entropy'],
@@ -30,8 +40,8 @@ class random_forest:
                         'max_features':[2,3,4],}
             grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=cv, n_jobs=-1)
             grid_search.fit(X_train, Y_train)
-            return grid_search.fit(X_train, Y_train)
-            
-    trained_Random_forest=grid_search(X_train,Y_train)
+            return grid_search.best_estimator_
+
+        trained_Random_forest = gridsearch(X_train, Y_train)
+        return trained_Random_forest
     
-    return trained_Random_forest
