f84045df9902e67f314044d68d9ca264b53e68dc
noreasjunnesson <160476163+noreasjunnesson@users.noreply.github.com>
2024-02-19 15:00:41 +0100

    Update naive.py

--- Models/naive.py
+++ Models/naive.py
@@ -1,1 +1,9 @@
-
+class naive:
+    def predict(self,X_test):
+        prediction_naive = np.zeros(len(X_test))  # Initialize a 1D array for predictions
+        for i, distance in enumerate(X_test['distances']):
+            if distance < 0.7:
+                prediction_naive[i] = 1
+            else:
+                prediction_naive[i] = 0
+        return prediction_naive
