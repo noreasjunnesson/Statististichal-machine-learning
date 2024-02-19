f12427b28616bb9bacef6f713abdcfc861b2db77
noreasjunnesson <160476163+noreasjunnesson@users.noreply.github.com>
2024-02-19 16:49:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from QDA import qda
 from Random_forest import random_forest
 from Logistic_regression import logistic_regression
 from Naive import naive
+from K_nearest import k_nearest
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
@@ -16,7 +17,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import KFold, GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
 from scipy.stats import randint
-
+from sklearn.neighbors import KNeighborsClassifier
 
 
 def load_data(filename):
@@ -84,67 +85,61 @@ def main():
     model_Random_forest = random_forest()
     model_Logistic_regression=logistic_regression()
     model_naive=naive()
+    model_knn=k_nearest()
 
     # Train models
     trained_model_QDA = model_QDA.train(X_train,Y_train)
     trained_model_Random_forest = model_Random_forest.train(X_train,Y_train)
     trained_model_Logistic_regression=model_Logistic_regression.train(X_train,Y_train)
+    trained_model_knn = model_knn.train(X_train,Y_train)
 
     #Predictions models
     prediction_QDA=trained_model_QDA.predict(X_test)
     prediction_Random_forest=trained_model_Random_forest.predict(X_test)
     prediction_Logistic_regression=trained_model_Logistic_regression.predict(X_test)
     prediction_naive=model_naive.predict(X_test)
+    prediction_knn=trained_model_knn.predict(X_test)
   
     #Accuray of models
     accuray_QDA=np.mean(prediction_QDA == Y_test)
     accuray_Random_forest=np.mean(prediction_Random_forest == Y_test)
     accuray_Logistic_regression=np.mean(prediction_Logistic_regression == Y_test)
     accuray_naive=np.mean(prediction_naive == Y_test)
+    accuray_knn=np.mean(prediction_knn == Y_test)
     
     #F1 score of models
     f1_QDA = f1_score(Y_test,prediction_QDA)
     f1_Random_forest = f1_score(Y_test,prediction_Random_forest)
     f1_Logistic_regression = f1_score(Y_test,prediction_Logistic_regression)
     f1_naive = f1_score(Y_test,prediction_naive)
+    f1_knn = f1_score(Y_test,prediction_knn)
 
     #Recall score of models
     recall_QDA = recall_score(Y_test,prediction_QDA)
     recall_Random_forest = recall_score(Y_test,prediction_Random_forest)
     recall_Logistic_regression = recall_score(Y_test,prediction_Logistic_regression)
     recall_naive = recall_score(Y_test,prediction_naive)
+    recall_knn = recall_score(Y_test,prediction_knn)
 
     #Precision score of models
     precision_QDA = precision_score(Y_test,prediction_QDA)
     precision_Random_forest = precision_score(Y_test,prediction_Random_forest)
     precision_Logistic_regression = precision_score(Y_test,prediction_Logistic_regression)
     precision_naive = precision_score(Y_test,prediction_naive)
-
-
-    # Compare results or perform further analysis
-    # For example:
-    print("Accuracy QDA:", accuray_QDA)
-    print("Accuracy Random forest:", accuray_Random_forest)
-    print("Accuracy Logistic regression:", accuray_Logistic_regression)
-    print("F1 score QDA", f1_QDA)
-    print("F1 score Random forest", f1_Random_forest)
-    print("F1 score Logistic regression", f1_Logistic_regression)
-    print("Recall score QDA",recall_QDA)
-    print("Recall score Random forest",recall_Random_forest)
-    print("Recall score Logistic regression",recall_Logistic_regression)
-    print("Precision score QDA",precision_QDA)
-    print("Precision score Random forest",precision_Random_forest)
-    print("Precision score Logistic regression",precision_Logistic_regression)
+    precision_knn = precision_score(Y_test,prediction_knn)
+ 
     data = {
-    'Model': ['QDA', 'Random Forest', 'Logistic Regression', 'Naive'],
-    'Accuracy': [accuray_QDA, accuray_Random_forest, accuray_Logistic_regression, accuray_naive],
-    'F1 Score': [f1_QDA, f1_Random_forest, f1_Logistic_regression, f1_naive],
-    'Recall': [recall_QDA, recall_Random_forest, recall_Logistic_regression, recall_naive],
-    'Precision': [precision_QDA, precision_Random_forest, precision_Logistic_regression, precision_naive]
+    'Model': ['QDA', 'Random Forest', 'Logistic Regression','KNN','Naive'],
+    'Accuracy': [accuray_QDA, accuray_Random_forest, accuray_Logistic_regression,accuray_knn ,accuray_naive],
+    'F1 Score': [f1_QDA, f1_Random_forest, f1_Logistic_regression,f1_knn, f1_naive],
+    'Recall': [recall_QDA, recall_Random_forest, recall_Logistic_regression,recall_knn, recall_naive],
+    'Precision': [precision_QDA, precision_Random_forest, precision_Logistic_regression,precision_knn, precision_naive]
     }
 
     df = pd.DataFrame(data)
 
+    # Print the DataFrame
+    print(df)
     # Print the DataFrame
     print(df)
 
