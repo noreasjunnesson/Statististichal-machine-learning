6460ab7a1e6da0e64656b1574d52ddf233ca6ffa
Tyraohrnberg <160476095+Tyraohrnberg@users.noreply.github.com>
2024-02-19 12:39:45 +0100

    Update Logistic_regression.py

--- Models/Logistic_regression.py
+++ Models/Logistic_regression.py
@@ -6,7 +6,7 @@ class logistic_regression:
     'C': np.logspace(-3, 3, 7),  # regularization parameter
     'solver': ['lbfgs'],  # optimization algorithm
     'penalty': ["l1","l2"] #l1 lasso, l2 ridge
-    grid_search = GridSearchCV(log_reg, param_grid, cv=5, scoring='accuracy')
+    grid_search = GridSearchCV(log_reg, param_grid, cv=cv, scoring='accuracy')
     grid_search.fit(X_train, Y_train)
     trained_log_reg = grid_search.best_estimator_
     return trained_logreg
