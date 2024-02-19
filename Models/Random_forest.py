04a0984d41df7c1c728bcf5e1d06226ef27fc304
noreasjunnesson <160476163+noreasjunnesson@users.noreply.github.com>
2024-02-19 10:37:43 +0100

    Update Random_forest.py

--- Models/Random_forest.py
+++ Models/Random_forest.py
@@ -2,4 +2,4 @@ class random_forest:
     def train(self, X_train,Y_train):
         # Train the model using train_data
         # Return the trained model
-        return trained_model
+        return random_forest
